aaa48cf1dd1a22538243d652637c835c327e7ee9
fatima-ezzahraalouane <falouane38@gmail.com>
2025-01-14 18:52:40 +0100

    #4 creer table role pour gerer les roles

--- SQL/script.sql
+++ SQL/script.sql
@@ -1,3 +1,8 @@
 CREATE DATABASE youdemy;
 
 USE youdemy;
+
+CREATE TABLE role (
+    id_role int NOT null AUTO_INCREMENT PRIMARY KEY,
+    name_user ENUM('Admin', 'Étudiant', 'Enseignant') NOT NULL
+);
