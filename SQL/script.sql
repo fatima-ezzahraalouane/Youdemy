40c92d7518ca3f1623c0a540bb1bc4e01a0c944d
fatima-ezzahraalouane <falouane38@gmail.com>
2025-01-15 00:34:16 +0100

    #5 creer la table usersite qui stocke les informations de tous les utilisateurs

--- SQL/script.sql
+++ SQL/script.sql
@@ -6,3 +6,15 @@ CREATE TABLE role (
     id_role int NOT null AUTO_INCREMENT PRIMARY KEY,
     name_user ENUM('Admin', 'Étudiant', 'Enseignant') NOT NULL
 );
+
+CREATE TABLE usersite (
+    id_usersite INT NOT null AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL UNIQUE,
+    id_role INT NOT NULL,
+    statut ENUM('actif', 'inactif', 'suspendu') DEFAULT 'actif', -- Statut du compte
+    is_approved BOOLEAN DEFAULT FALSE, -- Indique si le compte de l'enseignant est approuvé (TRUE) ou en attente (FALSE)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_role) REFERENCES role(id_role)
+);
